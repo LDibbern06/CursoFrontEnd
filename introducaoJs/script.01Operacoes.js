510adb71ce327c06761a74bf69ee74951e0f8497
LDibbern06 <lucasdibbern1992@gmail.com>
2025-02-06 11:28:13 -0300

    introducaoJavaEscript.js

---
+++ introducaoJs/script.01Operacoes.js
@@ -0,0 +1,36 @@
+//tipos de operacoes em JavaEscript
+
+let a = 10;
+let b = 3;
+
+console.log("soma")
+console.log(a+b);
+console.log("subtração")
+console.log(a-b);
+console.log("Multiplicação")
+console.log(a*b);
+console.log("Divisão")
+console.log(a/b);
+console.log("Resto da Divisão")
+console.log(a%b);
+
+//operadores Comparação ( >, >=, <, <=, ==, ===)
+console.log("Operadores relacionais")
+console.log(5>10) //false
+console.log(10 == "10"); //true
+console.log(10 === "10"); //false
+
+//Operadores logicos
+
+let nota1 = 5;
+let nota2 = 8;
+
+// our (||)
+console.log(nota1>7 || nota2>7); //false
+
+// and (&&)
+console.log(nota1>=7 && nota2 >=7)// flase
+
+//not(!)
+console.log(!true); //false
+
---
+++ introducaoJs/script00.js
@@ -0,0 +1,33 @@
+const texto = "Alo, Mundo"
+var boasVindas = "Bem-Vindo, Aluno(a)"
+boasVindas = "Boas Vindas Alunos"
+//texto = "ghjk"
+
+console.log(texto);
+console.log(boasVindas)
+
+//tipos de Dados
+
+let string = "Variavel de Texto"; //Sempre entre Aspas simples ou duplas
+let numero1 = 42;//nunero inteiro
+let numero2 = 10.65;//numero decimal
+let numero3 = "2";//numero em texto
+let boolean = true;
+let boolean2 = false;
+let boolean3 = "true";//texto
+let undefined;
+let nula = null;
+
+//let, var e const
+let resultado = true; //atribuição
+resultatdo  = false; //reatribuição
+//let resultado = true; - proibido escrever 
+
+var nota = 10;//atribuição 
+nota = 9;//reatribuição
+var nota = 8;//reescrita
+
+const fixo = 42;
+//fixo = 43; erro - nao posso reatribuir
+//const fixo = 41 = erro
+
